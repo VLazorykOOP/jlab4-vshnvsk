beaf2f0ea6376e897b702705a91a34bc31d5e70c
vshnvsk <vyshnovska.yana@chnu.edu.ua>
2023-04-03 22:25:37 +0300

    done task 2

---
+++ src/Task2.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+public class Task2 {
+    public static void main(String[] args) throws IOException{
+        Scanner in = new Scanner(System.in);
+
+        String inputPath = null;
+
+        while (inputPath == null || !new File(inputPath).isFile()){
+            System.out.println("Enter the file path:");
+            inputPath = in.nextLine();
+            if(!new File(inputPath).isFile()) {
+                System.out.println("The file does not exist(");
+            }
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputPath))){
+            int c;
+            System.out.println("Information from file: ");
+            while((c = reader.read()) != -1){
+                System.out.print((char)c);
+            }
+        } catch (IOException e){
+            System.out.println("Error reading data from file: " + e.getMessage());
+        }
+
+        System.out.println();
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputPath))){
+            String line;
+            System.out.println("Result: ");
+            while ((line = reader.readLine()) != null){
+                String[] words = line.split(" ");
+                for (String word : words){
+                    System.out.print(word);
+                }
+            }
+        } catch (IOException e){
+            System.out.println("Error reading data from file: " + e.getMessage());
+        }
+    }
+}
