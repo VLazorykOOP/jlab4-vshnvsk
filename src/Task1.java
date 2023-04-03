e15e91345764d4ec94df3e41a4722f43388f07b1
vshnvsk <vyshnovska.yana@chnu.edu.ua>
2023-04-03 13:09:27 +0300

    done task 1

---
+++ src/Task1.java
@@ -0,0 +1,74 @@
+import java.util.*;
+import java.io.*;
+public class Task1 {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+
+        Scanner file = null;
+        PrintWriter noNumber = null;
+        PrintWriter sortedNumberOutput = null;
+        String inputPath = null;
+
+        while (inputPath == null || !new File(inputPath).isFile()){
+            System.out.println("Enter the file path:");
+            inputPath = in.nextLine();
+            if(!new File(inputPath).isFile()) {
+                System.out.println("The file does not exist(");
+            }
+        }
+
+        List<Integer> numbers = new ArrayList<>();
+        List<String> letters = new ArrayList<>();
+        try(BufferedReader fileReader = new BufferedReader(new FileReader(inputPath))){
+            String line;
+            while ((line = fileReader.readLine()) != null){
+                boolean hasNumber = false;
+                int sum = 0;
+                StringBuilder stringBuilder = new StringBuilder();
+                for(int i = 0; i < line.length(); i++){
+                    char c = line.charAt(i);
+                    if(Character.isDigit(c)){
+                        hasNumber = true;
+                        stringBuilder.append(c);
+                    } else {
+                        if(stringBuilder.length() > 0){
+                            int number = Integer.parseInt(stringBuilder.toString());
+                            sum += number;
+                            numbers.add(number);
+                            stringBuilder = new StringBuilder();
+                        }
+                    }
+                }
+                if(stringBuilder.length() > 0){
+                    int number = Integer.parseInt(stringBuilder.toString());
+                    sum += number;
+                    numbers.add(number);
+                }
+                if(hasNumber){
+                    System.out.println(line + " (sum: " + sum + ")");
+                } else {
+                    letters.add(line);
+                }
+            }
+        }catch (IOException e) {
+            System.out.println("Error reading data from file: " + e.getMessage());
+        }
+
+        Collections.sort(numbers);
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(inputPath))){
+            for (int number : numbers) {
+                writer.write(number + " ");
+            }
+        } catch (IOException e){
+            System.out.println("Error reading data from file: " + e.getMessage());
+        }
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("D:\\2 курс\\Java\\letter.txt"))){
+            for(String line : letters){
+                writer.write(line + " ");
+            }
+        } catch (IOException e){
+            System.out.println("Error reading data from file: " + e.getMessage());
+        }
+    }
+}
